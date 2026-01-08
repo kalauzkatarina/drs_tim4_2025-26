36c3e5a5958dfbe8ea1b6f439af3aff83ab832c8
kalauzkatarina <kalauzkatarina39@gmail.com>
2026-01-08 21:02:21 +0100

    create, update operations, service, DTO and routers are created
    tested with thunder client <3

---
+++ services/server/DTO/UserDTO.py
@@ -0,0 +1,32 @@
+class UserCreateDTO:
+    def __init__(self, data: dict):
+        self.email = data.get("email")
+        self.password = data.get("password")
+        self.firstName = data.get("firstName")
+        self.lastName = data.get("lastName")
+        self.dateOfBirth = data.get("dateOfBirth")
+        self.gender = data.get("gender")
+        self.state = data.get("state")
+        self.streetName = data.get("streetName")
+        self.streetNumber = data.get("streetNumber")
+        self.accountBalance = data.get("accountBalance", 0.0)
+
+    def is_valid(self):
+        return all([
+            self.email,
+            self.password,
+            self.firstName,
+            self.lastName,
+            self.dateOfBirth
+        ])
+    
+class UserResponseDTO:
+    def __init__(self, user):
+        self.id = user.id
+        self.email = user.email
+        self.firstName = user.firstName
+        self.lastName = user.lastName
+        self.role = user.userRole.name
+
+    def to_dict(self):
+        return self.__dict__
