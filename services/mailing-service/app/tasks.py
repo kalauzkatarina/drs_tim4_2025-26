7ba64484c961ffb3fc4ff72b80a7f4d2805714bb
Filip Velemir <velemirfilip@gmail.com>
2026-01-31 17:54:08 +0100

    Update tasks.py

--- services/mailing-service/app/tasks.py
+++ services/mailing-service/app/tasks.py
@@ -11,31 +11,37 @@ def send_email(to,subject,body):
 
 
 @celery.task(name="email.task.send_ticket_cancelled_email")
-def send_ticket_cancel_email(userId,ticketId):
+def send_ticket_cancel_email(userId,flightId):
         # GET user
         user_resp = requests.get(f"http://localhost:5001/api/users/{userId}")
         user_data = user_resp.json()
         recipients = [user_data["email"]]
 
         # GET ticket (opciono, samo za info u mejlu)
-        ticket_resp = requests.get(f"http://localhost:5002/api/tickets/{ticketId}")
-        ticket_data = ticket_resp.json()
+        flight_resp = requests.get(f"http://localhost:5002/api/flights/{flightId}")
+        flight_data = flight_resp.json()
 
         msg = Message(
                 "Ticket cancelled",
                 sender=Config.MAIL_USERNAME,
                 recipients=recipients,
-                body=f"Your ticket #{ticketId} has been cancelled."
+                body=f"Your ticket for flight: {flight_data["name"]} has been cancelled."
         )
         mail.send(msg)
 
 @celery.task(name="email.task.send_flight_cancelled_email")
 def send_flight_cancelled_email(flightId):
-        tickets = requests.get(f"http://127.0.0.1:5002/api/tickets/flights-tickets/{flightId}")
+        ticket_response = requests.get(f"http://127.0.0.1:5002/api/tickets/flights-tickets/{flightId}")
+        ticket_data = ticket_response.json()
+        for ticket in ticket_data:
+                user_response = requests.get(f"http://127.0.0.1:5001/api/users/{ticket['userId']}",timeout=5)
 
-        for ticket in tickets:
-                user = requests.get(f"http://127.0.0.1:5001/api/users/{ticket.userId}")
+                user_data = user_response.json()
 
-                msg = Message("Flight cancelled",sender=Config.MAIL_USERNAME,recipients=user.email,body=f"Your flight {flightId} has been cancelled.\nYour ticket {ticket.id} is no longer valid.")
+                msg = Message("Flight cancelled",sender=Config.MAIL_USERNAME,recipients=[user_data['email']],body=(
+                    f"Your flight {flightId} has been cancelled.\n"
+                    f"Your ticket #{ticket['id']} "
+                    f"(price: {ticket['ticketPrice']}) is no longer valid."
+                ))
 
                 mail.send(msg)
